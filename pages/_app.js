b205bfd1f375ac543636ee7a68a684b1c69e33c3
Enzo Vergara <enzoamb123@gmail.com>
2020-12-25 14:11:15 -0300

    avances enami 2.1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import 'normalize.css'
 import '../styles/style.scss'
 import '../node_modules/enami/dist/enami.min.css'
-import '../../enami/dist/enami.min.css'
+// import '../../enami/dist/enami.min.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
