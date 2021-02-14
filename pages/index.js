f366e1af43a306f374dc53b6dc3c0b178ab2be86
Enzo Vergara <enzoamb123@gmail.com>
2021-02-14 12:03:44 -0300

    Update favicon

--- pages/index.js
+++ pages/index.js
@@ -27,16 +27,15 @@ export default function Home() {
 
       <Head>
         <title>enami.js — a simple animation-on-scroll library</title>
-        {/* <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏄‍♂️</text></svg>" /> */}
         <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png" />
         <link rel="manifest" href="img/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#002665" />
+        <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#00143b" />
         <link rel="shortcut icon" href="img/favicon/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#002665" />
+        <meta name="msapplication-TileColor" content="#00143b" />
         <meta name="msapplication-config" content="img/favicon/browserconfig.xml" />
-        <meta name="theme-color" content="#002665" />
+        <meta name="theme-color" content="#00143b" />
 
       </Head>
 
